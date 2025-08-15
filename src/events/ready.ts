f91f0ffeccd5ae0372484d34eface49d92b87306
SirNotEthan <ethn.bannister15@gmail.com>
2025-08-15 11:51:00 +0100

    Ready Event Structure Fix

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,14 +1,10 @@
 import { Events, Client, ActivityType } from 'discord.js';
 
-export default {
-    name: Events.ClientReady,
-    once: true,
-    async execute(client: Client) {
-        console.log(`Logged in as ${client.user?.tag}!`);
-        
-        await client.user?.setActivity('your server', { type: ActivityType.Watching });
-    },
-    async onError(error: Error) {
-        console.error('An error occurred:', error);
-    }
+export const name = Events.ClientReady;
+export const once = true;
+
+export async function execute(client: Client<true>) {
+    console.log(`Logged in as ${client.user?.tag}!`);
+    
+    client.user?.setActivity('Admiring the wonderful work', { type: ActivityType.Custom });
 }
